27ebb2aca99e0d624dacc7bdd66343d79f9fe65b
Lik <marcustlw@hotmail.com>
2021-08-26 01:09:17 +0800

    idk whats wrong suddenly

---
+++ client/src/components/TimePicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="block">
+    <span class="demonstration">Default</span>
+    <el-date-picker
+      v-model="value1"
+      type="datetime"
+      placeholder="Select date and time">
+    </el-date-picker>
+  </div>
+</template>
+
+<script>
+  
+  export default defineComponent({
+     name: 'TimePicker',
+     
+  });
+</script>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,10 +1,45 @@
 <template>
-    <div class="dashboard"></div>
+    <div class="dashboard">
+        <div class="activity-box">
+            <h1>Create Your Workout</h1>
+            <p>
+            <br><br>Exercise Type: Running
+            </p>
+
+            <br><div class="timepicker">
+             <TimePicker/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import {TimePicker} from "@/components/TimePicker"
 export default {
     name: 'Dashboard',
-    components: {},
-};
+    components: {TimePicker},
+}
 </script>
+
+<style lang="scss" scoped>
+.activity-box {
+  padding: 50px 5%;
+  background-color: #E0EEEE;
+  height: 500px;
+  
+  .timepicker {
+    display: grid;
+    justify-items: center;
+    grid-row-gap: 23px;
+    width: 100%;
+    padding-top: 25px;
+   }
+
+  .controls {
+  display: flex;
+  justify-content: space-between;
+
+  width: 187px;
+}
+}
+</style>
