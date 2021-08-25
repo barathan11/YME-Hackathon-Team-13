b85ff106ba80cc86c1e9315763ff004853abf820
NitinMa2 <nitinmathew.official@gmail.com>
2021-08-25 18:28:53 +0800

    setting up starting code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+    <div>
+        <Navigation />
+        <router-view />
+    </div>
 </template>
 
+<script>
+import Navigation from '@/components/Navigation';
+export default {
+    name: 'App',
+    components: {
+        Navigation,
+    },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    transition: all 0.5s;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+        >
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-menu-item index="2">
+                <a href="https://www.ele.me" target="_blank">Orders</a>
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const activeIndex = ref('1');
+        const handleSelect = (key, keyPath) => {
+            console.log(key, keyPath);
+        };
+        return {
+            activeIndex,
+            handleSelect,
+        };
+    },
+});
+</script>
