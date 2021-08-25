8b67068184e8d2976d306ab95aa4aff4d4f4020b
NitinMa2 <nitinmathew.official@gmail.com>
2021-08-26 01:24:26 +0800

    adding timepicker

--- client/src/components/TimePicker.vue
+++ client/src/components/TimePicker.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="block">
-    <span class="demonstration">Default</span>
-    <el-date-picker
-      v-model="value1"
-      type="datetime"
-      placeholder="Select date and time">
-    </el-date-picker>
-  </div>
+    <div>
+        <div class="block">
+            <el-date-picker
+                v-model="value1"
+                type="datetime"
+                placeholder="Select date and time"
+            ></el-date-picker>
+        </div>
+    </div>
 </template>
 
 <script>
-  
-  export default defineComponent({
-     name: 'TimePicker',
-     
-  });
+import { defineComponent, reactive, toRefs } from 'vue';
+
+export default defineComponent({
+    name: 'TimePicker',
+    setup() {
+        const state = reactive({
+            value1: '',
+        });
+
+        return {
+            ...toRefs(state),
+        };
+    },
+});
 </script>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -3,43 +3,46 @@
         <div class="activity-box">
             <h1>Create Your Workout</h1>
             <p>
-            <br><br>Exercise Type: Running
+                <br />
+                <br />
+                Exercise Type: Running
             </p>
 
-            <br><div class="timepicker">
-             <TimePicker/>
+            <br />
+            <div class="timepicker">
+                <TimePicker />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import {TimePicker} from "@/components/TimePicker"
+import TimePicker from '@/components/TimePicker';
 export default {
     name: 'Dashboard',
-    components: {TimePicker},
-}
+    components: { TimePicker },
+};
 </script>
 
 <style lang="scss" scoped>
 .activity-box {
-  padding: 50px 5%;
-  background-color: #E0EEEE;
-  height: 500px;
-  
-  .timepicker {
-    display: grid;
-    justify-items: center;
-    grid-row-gap: 23px;
-    width: 100%;
-    padding-top: 25px;
-   }
+    padding: 50px 5%;
+    background-color: #e0eeee;
+    height: 500px;
 
-  .controls {
-  display: flex;
-  justify-content: space-between;
+    .timepicker {
+        display: grid;
+        justify-items: center;
+        grid-row-gap: 23px;
+        width: 100%;
+        padding-top: 25px;
+    }
 
-  width: 187px;
-}
+    .controls {
+        display: flex;
+        justify-content: space-between;
+
+        width: 187px;
+    }
 }
 </style>
